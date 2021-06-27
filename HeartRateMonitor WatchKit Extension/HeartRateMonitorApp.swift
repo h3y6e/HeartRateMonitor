6b85d52fd202c3928ba4eeed0f2617510b888f35
5ebec <hash.a5ebec@gmail.com>
2021-06-27 18:19:32 +0900

    Initial Commit

---
+++ HeartRateMonitor WatchKit Extension/HeartRateMonitorApp.swift
@@ -0,0 +1,19 @@
+//
+//  HeartRateMonitorApp.swift
+//  HeartRateMonitor WatchKit Extension
+//
+//  Created by Hiroya Kawase on 2021/06/27.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
